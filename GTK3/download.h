c61494a4adbb3064227e92840e104328b9c441d2
Jason Lethbridge <lethbridgejason@gmail.com>
2018-05-13 13:59:34 +1000

    Added download functions into their own file

---
+++ GTK3/download.h
@@ -0,0 +1,7 @@
+extern GtkWindow * InitDownloadPrompt(gchar * fn);
+
+extern void c_download_start(WebKitWebContext * wv
+    ,WebKitDownload * dl, void * v);
+
+static gboolean c_download_name(WebKitDownload * d, gchar * fn
+	,void * v);
