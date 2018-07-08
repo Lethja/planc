939e0a43c76b83038eab2dc71cb927ffea95e664
Jason Lethbridge <lethbridgejason@gmail.com>
2018-07-09 01:53:21 +1000

    Fixed being able to hide the traditional menu in desktop enviroment that dosen't prefer a Gmenu

--- settings.c
+++ settings.c
@@ -29,7 +29,8 @@ void c_notebook_tabs_autohide(GtkToggleButton * cbmi
 void c_traditional_menu_hide(GtkToggleButton * cbmi
 	,void * v)
 {
-	gboolean setting = gtk_toggle_button_get_active(cbmi);
+	gboolean setting = (gtk_toggle_button_get_active(cbmi)
+		|| !preferGmenu());
 	GList * wins = gtk_application_get_windows(G_APP);
 	while(wins)
 	{
