f3f3c90bfc886f950ca218ce8850f682f2013e76
Jason Lethbridge <lethbridgejason@gmail.com>
2020-06-28 13:47:41 +1000

    Added extension context menu function

--- libpolicy.c
+++ libpolicy.c
@@ -49,12 +49,46 @@ static gboolean c_webpage_send_request(WebKitWebPage * wp
 	return FALSE;
 }
 
+static gboolean c_webpage_context(WebKitWebPage * wp
+	,WebKitContextMenu * cm, WebKitWebHitTestResult *hr, void * v)
+{
+	g_autofree char *string = NULL;
+	GVariantBuilder builder;
+	WebKitFrame *frame;
+	g_autoptr (JSCContext) js_context = NULL;
+	g_autoptr (JSCValue) js_value = NULL;
+
+	frame = webkit_web_page_get_main_frame (wp);
+	js_context = webkit_frame_get_js_context(frame);
+
+	js_value = jsc_context_evaluate (js_context
+		,"window.getSelection().toString();", -1);
+	if (!jsc_value_is_null (js_value)
+	&& !jsc_value_is_undefined (js_value))
+		string = jsc_value_to_string (js_value);
+
+	if (!string || *string == '\0')
+		return FALSE;
+
+	g_variant_builder_init (&builder, G_VARIANT_TYPE ("a{sv}"));
+	g_variant_builder_add (&builder, "{sv}", "SelectedText"
+		,g_variant_new_string (g_strstrip (string)));
+	webkit_context_menu_set_user_data (cm
+		,g_variant_builder_end (&builder));
+#ifndef NDEBUG
+	g_debug("Page selection is %s", string);
+#endif
+	return TRUE;
+}
+
 static void
 web_page_created_callback (WebKitWebExtension *extension,
 	WebKitWebPage * wp, gpointer user_data)
 {
 	g_signal_connect (wp, "send-request"
 		,G_CALLBACK (c_webpage_send_request), NULL);
+	g_signal_connect (wp, "context-menu"
+		,G_CALLBACK (c_webpage_context), NULL);
 }
 
 G_MODULE_EXPORT void
